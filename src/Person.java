08d5c8b222347133a452aa49c323a4ad96c76626
Makswik <iblochko888@mail.ru>
2023-09-27 10:29:03 +0300

    add

--- src/Person.java
+++ src/Person.java
@@ -1,24 +1,19 @@
 public class Person {
     private String name;
     private int age;
-
     public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+        this.name = Maksim;
+        this.age = 10;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
